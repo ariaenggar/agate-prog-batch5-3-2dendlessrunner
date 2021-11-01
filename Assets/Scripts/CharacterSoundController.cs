f692fd93285c5c9cd5b442116a8ce9781753c9d0
ariaenggar <aria.enggar@gmail.com>
2021-11-01 21:51:16 +0700

    init

---
+++ Assets/Scripts/CharacterSoundController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundController : MonoBehaviour
+{
+    private AudioSource audioPlayer;
+ 
+    public AudioClip jump;
+
+    public AudioClip scoreHighlight;
+
+    public void PlayScoreHighlight()
+    {
+        audioPlayer.PlayOneShot(scoreHighlight);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void PlayJump()
+    {
+        audioPlayer.PlayOneShot(jump);
+    }
+}
